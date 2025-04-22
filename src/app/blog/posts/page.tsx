b7264552bceac7c1dc22d4d68cb9d965e469ef03
Carlos Gomez <gomezmendozacarlos@gmail.com>
2025-04-22 15:53:47 -0600

    TS error cleaned

--- src/app/blog/posts/page.tsx
+++ src/app/blog/posts/page.tsx
@@ -1,13 +1,14 @@
-import { posts } from '@/app/lib/placeholder-data';
 import Post from '@/app/ui/components/posts/Post';
-import  { connectToDb } from '@/app/lib/data'
+import  { connectToDb, getPosts } from '@/app/lib/data'
 
 export default async function Page() {
   const client = await connectToDb()
+  const posts = await getPosts()
+ 
   return (
     <>
       {client && <p className='text-green-500'>Connected to the database</p>}
       <h1>Posts</h1>
-      {posts.map((post) => <Post key={post.id} {...post} />)}
+      {posts.map((post) => <Post key={post.id} id={post.id} title={post.title} content={post.content} date={post.date} />)}
     </>)
 }
