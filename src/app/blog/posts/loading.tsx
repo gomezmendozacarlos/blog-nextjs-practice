1c5e7db4596f1341a9d68ce31f145fade1319487
Carlos Gomez <gomezmendozacarlos@gmail.com>
2025-04-22 21:14:53 -0600

    Adds skeleton components into loading page

--- src/app/blog/posts/loading.tsx
+++ src/app/blog/posts/loading.tsx
@@ -1,7 +1,17 @@
-export default function Loading() {
+import { PostsSkeleton } from "@/app/ui/components/skeletons"
+
+/* export default function Loading() {
   return (
     <div className="loading">
       <h2>Loading...</h2>
     </div>
   )
-}
+} */
+
+  export default function Loading() {
+    return (
+      <>
+        <PostsSkeleton />
+      </>
+    )
+  }
