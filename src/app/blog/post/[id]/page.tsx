fdb321b140654a412a87528d66e52d61e407a94f
Carlos Gomez <gomezmendozacarlos@gmail.com>
2025-04-28 18:18:31 -0600

    adds not found template to post page

--- src/app/blog/post/[id]/page.tsx
+++ src/app/blog/post/[id]/page.tsx
@@ -1,8 +1,25 @@
-import { posts } from '@/app/lib/placeholder-data';
+import { notFound } from 'next/navigation';
 import Post from '@/app/ui/components/posts/Post';
+import { connectToDb, getPosts } from '@/app/lib/data';
+//import { posts } from '@/app/lib/placeholder-data';
 
-export default function Page({ params }: { params: { id: string } }) {
-  const post = posts.find((post) => post.id === params.id);
+type Post =   {
+  id: string;
+  title: string;
+  content: string;
+  date: string; 
+  user: string;
+}
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const posts = await getPosts();
+  
+  const post = posts.find((post) => post.id === params.id) as Post | undefined;
+  
+  if (!post) {
+    notFound();
+  }
+
   return (
     <>
       <h1>Post</h1>
--- src/app/ui/components/posts/Post.tsx
+++ src/app/ui/components/posts/Post.tsx
@@ -1,9 +1,11 @@
 import React, { JSX } from 'react';
+import Link from 'next/link';
 
 export default function Component({ id, title, content, date }: { id: string, title: string, content: string, date: string }) {
+    const url = `/blog/post/${id}`;
     return (
         <div key={id} className="border border-gray-200 p-4 my-4">
-            <h2>{title}</h2>
+            <Link href={url}><h2>{title}</h2></Link>
             <p className="text-gray-500">{date}</p>
             <p>{content}</p>
         </div>
